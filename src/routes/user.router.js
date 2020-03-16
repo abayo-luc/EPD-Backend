199e33e3558bf223a04ce44c62ff57dd543a61a7
Luc Abayo <luc.bayo@gmail.com>
2020-03-16 14:43:38 +0200

    Bg fix controller path (#14)
     :bug: fix user controller

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import UserController from "../controllers/UserController";
+import UserController from "../controllers/UsersController";
 import authenticate from "../middleware/authenticate";
 import authorize, { roleAssignable } from "../middleware/authorize";
 
