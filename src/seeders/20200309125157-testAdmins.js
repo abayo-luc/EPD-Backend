00d647a76d2c8e225c8b9fec51c133392bb897fe
Luc <luc.bayo@gmail.com>
2020-05-24 10:58:07 +0200

    add supervisor stats

--- src/seeders/20200309125157-testAdmins.js
+++ src/seeders/20200309125157-testAdmins.js
@@ -49,6 +49,20 @@ module.exports = {
           updatedAt: new Date()
         },
         {
+          id: "10b5f343-efbd-45ce-85d0-7f694a91d34c",
+          name: "Agent 2",
+          username: "agent2",
+          confirmed: true,
+          blocked: false,
+          email: "agent2@example.com",
+          phoneNumber: "0780000003",
+          password: CUSTOM_PASSWORD,
+          role: "agent",
+          createdAt: new Date(),
+          companyId: "53d8cfb2-b6a7-4e61-8088-ae278ef2e5e6",
+          updatedAt: new Date()
+        },
+        {
           id: uuid(),
           name: "Supervisor",
           username: "superv12",
--- src/seeders/20200516093316-testing-sales-records.js
+++ src/seeders/20200516093316-testing-sales-records.js
@@ -2,24 +2,36 @@ const faker = require("faker");
 
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
-    const data = [...Array(100).keys()].map(() => ({
-      id: faker.random.uuid(),
-      clientName: faker.name.firstName(),
-      phoneNumber: faker.phone.phoneNumber(),
-      province: "North",
-      district: "Gicumbi",
-      sector: "Byumba",
-      cell: "Gisuna",
-      village: "Gisuna",
-      editable: false,
-      sex: "male",
-      age: faker.random.number(),
-      clientID: faker.random.number(),
-      companyId: "53d8cfb2-b6a7-4e61-8088-ae278ef2e5e0",
-      userId: "10b5f343-efbd-45ce-85d0-7f694a91d34d",
-      createdAt: faker.date.between("01/01/2019", "12/30/2019"),
-      updatedAt: new Date()
-    }));
+    const data = [...Array(100).keys()].map((_item, index) => {
+      let i = 0;
+      if (index > 30) {
+        i = index % 2 === 0 ? 0 : 1;
+      }
+      return {
+        id: faker.random.uuid(),
+        clientName: faker.name.firstName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        province: "North",
+        district: "Gicumbi",
+        sector: "Byumba",
+        cell: "Gisuna",
+        village: "Gisuna",
+        editable: false,
+        sex: "male",
+        age: faker.random.number(),
+        clientID: faker.random.number(),
+        companyId: [
+          "53d8cfb2-b6a7-4e61-8088-ae278ef2e5e0",
+          "53d8cfb2-b6a7-4e61-8088-ae278ef2e5e6"
+        ][i],
+        userId: [
+          "10b5f343-efbd-45ce-85d0-7f694a91d34d",
+          "10b5f343-efbd-45ce-85d0-7f694a91d34c"
+        ][i],
+        createdAt: faker.date.between("06/06/2019", new Date()),
+        updatedAt: new Date()
+      };
+    });
     return queryInterface.bulkInsert("Sales", [...data], {});
   },
 
