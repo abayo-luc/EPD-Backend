700f601d0c05921db6ac4e6b72b10ddd6dffb4a0
Luc Abayo <luc.bayo@gmail.com>
2020-12-07 14:53:14 +0200

    chore: from postres to mssq (#44)
    Co-authored-by: Luc Abayo <luc.bayo@gmailcom>

--- src/utils/notifier.js
+++ src/utils/notifier.js
@@ -1,18 +1,18 @@
-import dotenv from "dotenv";
-import twilio from "twilio";
+// import dotenv from "dotenv";
+// import twilio from "twilio";
 
-dotenv.config();
+// dotenv.config();
 
-const {
-  TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN,
-  TWILIO_PHONE_NUMBER
-} = process.env;
-const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+// const {
+//   TWILIO_ACCOUNT_SID,
+//   TWILIO_AUTH_TOKEN,
+//   TWILIO_PHONE_NUMBER
+// } = process.env;
+// const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-export default ({ message, phoneNumber }) =>
-  client.messages.create({
-    to: `+25${phoneNumber}`,
-    from: TWILIO_PHONE_NUMBER,
-    body: message
-  });
+// export default ({ message, phoneNumber }) =>
+//   client.messages.create({
+//     to: `+25${phoneNumber}`,
+//     from: TWILIO_PHONE_NUMBER,
+//     body: message
+//   });
